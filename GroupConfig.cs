d69453a511b0d51ae68ad641fb669f486bdae984
Slcotix0 <129961545+Slcotix0@users.noreply.github.com>
2023-04-05 15:44:59 +0300

    Добавьте файлы проекта.

---
+++ GroupConfig.cs
@@ -0,0 +1,59 @@
+﻿// Написал Алексей Сырчин, спиздил Андрей Игошин
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FourElements
+{
+    // Сам разберешься
+    class ConfigEntry
+    {
+        public string GroupId;
+        public string GroupType;
+        public string[] Codes;
+    }
+
+    class GroupConfig
+    {
+        public ConfigEntry[] Data;
+
+        public void ReadConfig(string FileName)
+        {
+            StreamReader SR = new StreamReader(FileName);
+            string currentLine;
+            ConfigEntry e = new ConfigEntry();
+
+            currentLine = SR.ReadLine();
+
+            if (currentLine != null && !currentLine.Contains(";"))
+            {
+               
+                if (currentLine.Contains("-group"))
+                {
+                    if (e != null)
+                    {
+                        e = null;
+                    }
+                    else
+                    {
+                        e = new ConfigEntry();
+                    }
+                }
+
+                if (currentLine.Contains("-eog"))
+                {
+                    Data.Append<ConfigEntry>(e);
+                    e = null;
+                }
+
+                string[] t = currentLine.Split(':');
+
+            }
+
+            SR.Close();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿// Написал Алексей Сырчин, спиздил Андрей Игошин
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FourElements
+{
+    static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        /// 
+        public static GroupConfig Cfg;
+
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+
+            Cfg = new GroupConfig();
+        }
+
+    }
+}
